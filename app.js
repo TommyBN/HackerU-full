6d026ed9b0652e1f8721a62119d86f5fb3c8308b
Tom Ben Ner <tommybenner@hotmail.com>
2022-02-15 19:24:14 +0200

    last commit

--- app.js
+++ app.js
@@ -1,112 +1,11 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const data = require('./data.json')
+const lecturers = data.lecturers
+const languages = data.languages
+
 app.use(express.static('public'))
-
-const lecturers = [
-    {
-        id: 1,
-        name: "Moshe",
-        email: "moshe@moshe.com",
-        languages: [
-            1,
-            3,
-            9
-        ]
-    },
-    {
-        id: 20,
-        name: "Avi",
-        email: "avi@moshe.com",
-        languages: [
-            3
-        ]
-    },
-    {
-        id: 300,
-        name: "Gila",
-        email: "gila@moshe.com",
-        languages: [
-            1,
-            6,
-            8,
-            9
-        ]
-    },
-    {
-        id: 350,
-        name: "Yehonathan",
-        email: "Yehonathan@moshe.com",
-        languages: [
-            2,
-            4,
-            5,
-            8
-        ]
-    },
-    {
-        id: 23,
-        name: "Gamliel",
-        email: "Gamliel@moshe.com",
-        languages: [
-            2,
-            3,
-            7,
-            9
-        ]
-    },
-    {
-        id: 67,
-        name: "Shimon",
-        email: "Shimon@moshe.com",
-        languages: [
-            2,
-            4,
-            6,
-            7
-        ]
-    },
-]
-
-const languages = [
-    {
-        id: 1,
-        name: "Java"
-    },
-    {
-        id: 2,
-        name: ".NET"
-    },
-    {
-        id: 3,
-        name: "NodeJS"
-    },
-    {
-        id: 4,
-        name: "Advanced Vanilla JS"
-    },
-    {
-        id: 5,
-        name: "React"
-    },
-    {
-        id: 6,
-        name: "Angular"
-    },
-    {
-        id: 7,
-        name: "Kotlin"
-    },
-    {
-        id: 8,
-        name: "Dart"
-    },
-    {
-        id: 9,
-        name: "Flutter"
-    }
-]
-
 
 app.get('/lecturers', async (req, res) => {
     try {
